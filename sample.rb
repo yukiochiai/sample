95e424f2bdeb849d817e0a4aabd8904b25a43dc6
yukiochiai <ochiai.y41@gmail.com>
2020-07-13 22:04:22 +0900

    h

--- sample.rb
+++ sample.rb
@@ -1,5 +1,14 @@
-def hello
-  p "hello, GitHub Desktop."
+def search(target_num, input)
+
+  input.each_with_index do |num, index|
+
+    if num == target_num 
+       puts "#{index + 1}番目にあります"
+       return
+    end 
+  end
+  puts "その数は含まれていません"
 end
 
-hello
+input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
+search(11,input)
